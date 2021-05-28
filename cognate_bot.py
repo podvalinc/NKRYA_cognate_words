acbbd62e29ab2b67f2aa140304f36d647a723fbb
dj-kostya <k.nosorev@g.nsu.ru>
2021-05-28 19:21:35 +0700

    Fix token

--- cognate_bot.py
+++ cognate_bot.py
@@ -1,9 +1,11 @@
 # @Readability_test_bot
 import telebot
 from baseline import getEvristicCognate, init
+import os 
 # from books_n_kids import predict_text_class
-
-bot = telebot.TeleBot('1790787625:AAGY4fc0kyIiuiNhxUtTMnOqjZ8oSq3NXgM')
+token = os.environ.get('AUTH_TOKEN')
+print(token)
+bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(content_types=['text'])
@@ -11,7 +13,6 @@ def get_text_messages(message):
     if message.text == '/start':
         bot.send_message(message.from_user.id, 'Привет, отправь мне два слова и я скажу тебе, однокоренные они или нет')
     else:
-        
         words = message.text.lower().split()
         if not words or len(words) != 2:
             bot.send_message(message.from_user.id, 'Привет, отправь мне два слова')
